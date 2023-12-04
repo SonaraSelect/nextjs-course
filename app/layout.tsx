674c092c44538f3ae7a3385813c9bc0d165493ee
Sonara <93947986+SonaraSelect@users.noreply.github.com>
2023-12-04 16:15:33 -0500

    removing footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,34 +23,6 @@ export default function RootLayout({ children }: Props) {
           <div className="container">
             <NavMenu />
             <main>{children}</main>
-
-            <footer>
-              <p>
-                Created for the{' '}
-                <Link href="https://fireship.io">
-                  Fireship Next.js 13 Full Course
-                </Link>
-              </p>
-              <ul>
-                <li>
-                  <Link href={'/about'}>About</Link>
-                </li>{' '}
-                |
-                <li>
-                  <Link href={'https://www.youtube.com/@Fireship'}>
-                    YouTube
-                  </Link>
-                </li>{' '}
-                |
-                <li>
-                  <Link href={'/login'}>Source Code</Link>
-                </li>{' '}
-                |
-                <li>
-                  <Link href={'https://nextjs.org'}>NextJS Docs</Link>
-                </li>
-              </ul>
-            </footer>
           </div>
         </body>
       </html>
