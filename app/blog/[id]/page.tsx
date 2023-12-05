e808e8c4c9cd0a21cdece67d30a491c5274d26ac
Sonara <93947986+SonaraSelect@users.noreply.github.com>
2023-12-05 01:05:00 -0500

    schizo nullables

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -21,11 +21,11 @@ export default async function BlogPostPage({ params }: Props) {
   const posts: Post[] = await fetch('http://localhost:3000/api/contentpost').then(
     (res) => res.json()
   );
-  const post = posts.find((post) => post.id === params.id)!;
+  const post = posts?.find((post) => post?.id === params?.id)!;
 
   // Find user by email (authorid)
   const user = await prisma.user
-    .findUnique({ where: { email: post.authorid } });
+    .findUnique({ where: { email: post?.authorid } });
 
   const userLink = '/users/' + user?.id;
   
