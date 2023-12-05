2c5dfc80d738a5c3f318195fbeb65d07db128d4e
Sonara <93947986+SonaraSelect@users.noreply.github.com>
2023-12-05 02:19:42 -0500

    deleting blog

--- app/api/contentpost/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getServerSession } from 'next-auth';
-import { prisma } from '@/lib/prisma';
-
-export async function GET(request: Request) {
-  const posts = await prisma.posts.findMany();
-
-  return NextResponse.json(posts);
-}
